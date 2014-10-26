2347d134a577edd5e5c1203c4e844826d6936520
Danilo Scodellaro <dan.scodellaro@gmail.com>
2014-10-27 04:11:36 +1100

    4:11am

--- hunter.c
+++ hunter.c
@@ -13,6 +13,29 @@
 //Enable DEBUG mode (0 = FALSE, 1 = TRUE)
 #define DEBUG 0 
 
+
+// New Functions
+
+//Make a move at random from all possible moves   
+//If a valid move if found, function calls registerBestPlay()
+//Returns the locationID correpsonding to the Location Initials registered
+LocationID randomMove(HunterView gameState, LocationID possibleDestinations[], int numLocations);
+
+//Make a move if Dracula's current location is within the Hunter's possible moves 
+//If a valid move if found, function calls registerBestPlay()
+//Returns the locationID correpsonding to the Location Initials registered
+LocationID singleMove(HunterView gameState, LocationID possibleDestinations[], int numLocations);
+
+//Make a move that is the first step of the shortest path to dracula
+//If a valid move if found, function calls registerBestPlay()
+//Returns the locationID correpsonding to the Location Initials registered
+LocationID shortestMove(HunterView gameState, LocationID possibleDestinations[], int numLocations, Map europe);
+
+//Move to Dracula's last known location within the trail
+//If a valid move if found, function calls registerBestPlay()
+//Returns the locationID correpsonding to the Location Initials registered
+LocationID lastKnownMove(HunterView gameState, LocationID possibleDestinations[], int numLocations, Map europe);
+
 //"Main" Function
 void decideHunterMove(HunterView gameState)
 {
